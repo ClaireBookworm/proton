c005dca3a49d62cf380f510d44828001b859a054
Claire Wang <clara32356@gmail.com>
2019-09-01 21:15:18 -0700

    WORKS!

--- python.py
+++ python.py
@@ -1,7 +1,4 @@
-# https://www.geeksforgeeks.org/detection-specific-colorblue-using-opencv-python/
-
-# Python program for Detection of a  
-# specific color(blue here) using OpenCV with Python 
+# Proton Project for video green color detection -- Claire
 import sys
 import argparse
 import cv2 
@@ -10,69 +7,64 @@ import time
 import imageio
 from PIL import Image
 
-  
+# ENTER FILE NAME
 cap = cv2.VideoCapture('trim.mp4')  
-fps = cap.get(cv2.CAP_PROP_FPS)
+# calculates frame rate -- more accurate
+fps = cap.get(cv2.CAP_PROP_FPS) 
 
-count = 0
+print('Youre camera\'s FPS is: ' + str(fps))
+# should be 16.6 milliseconds / frame because it's 60 fps
+# frame_gap: time between each frame
+frame_gap = (1/fps) * 1000 
+fInterval = 5 
 
 def millTime (count, fps):
-    interval = 1.00/(fps/1000)
     # takes in count and converts to seconds
-    return float(count*interval)
+    return round(count*(1/fps), 2)
 
 def inRange(main, lower, upper):
     c = 0
     for num in range(0, 3):
-        # print("main: " + str(main[num]) + " upper: " + str(upper[num]) + " lower: " + str(lower[num]) + ".")
         if int(main[num]) <= int(upper[num]) and int (main[num]) >= int(lower[num]):
             c += 1
     return c >= 3
-# 116 125 124
 
 #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-sensitivity = 60;
-#lower_green = np.array([60 - sensitivity, 100, 100]) 
-#upper_green = np.array([60 + sensitivity, 255, 255])
-#lower_green = np.array([48, 138, 66]) 
-
-#lower_green = np.array([100, 200, 100]) 
-#upper_green = np.array([255, 255, 255])
-
+# GREEN COLOR DEFINITION
 lower_green = [100, 200, 100]
 upper_green = [255, 255, 255]
 
 success = True
+def extract (results, fps, limit):
+    count = 0 
+    # count is the # of frames we go through, when interval not 1, be careful!
+    while count < limit:
+        #cap.set(cv2.CAP_PROP_POS_MSEC,(count*16.6))    # 60 fps, 16.6
+        cap.set(cv2.CAP_PROP_POS_MSEC,(count*frame_gap))
+        success,image = cap.read()
+        
+        green = image[10, 10]
+        # print(green)
+        if inRange(green, lower_green, upper_green):
+            print ('Read frame %d: ' % count,success) # useless but helpful
+            time = millTime(count, 60)
+            print ("Time: %f" % time)
+            results.append(time)
+            # cv2.imwrite("images/" + "frame%d.jpg" % count, image) 
+        print ("Num: " + str(count))
+        print("TIMESTAMPS")
+        print(results)
+        count = count + fInterval
+    return count
 
 results = []
 
-while success:
-    #cap.set(cv2.CAP_PROP_POS_MSEC,(count*16.6))    # 60 fps, 16.6
-    cap.set(cv2.CAP_PROP_POS_MSEC,(count*249))
-    success,image = cap.read()
-    #im = Image.fromarray(image) # Can be many different formats.
-    #pix = im.load()
-    #green = pix[125, 125]
-    
-    green = image[10, 10]
-    # print(green)
-    if inRange(green, lower_green, upper_green):
-        print ('Read frame %d: ' % count,success)
-        time = millTime(count, 60)
-        print ("Time: %f" % time)
-        results.append(time)
-        # cv2.imwrite("images/" + "frame%d.jpg" % count, image) 
-    print ("Num: " + str(count))
-    print("TIMESTAMPS")
-    print(results)
-    count = count + 1
+def calc_limit (vid_length, fps):
+    return (vid_length * fps) # vid_length in seconds
 
-# coor = np.zeroes()
-  
-#k = cv2.waitKey(5) & 0xFF
-  
-# Destroys all of the HighGUI windows. 
-#cv2.destroyAllWindows() 
-  
-# release the captured frame 
-cap.release() 
+
+# CALC_LIMIT (video length in seconds, fps) -- fps stays as fps
+limit = calc_limit (60, fps)
+
+# EXTRACT(results array name, fps, limit) -- limit from above
+extract (results, fps, limit)
